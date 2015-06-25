e45db5e73b36a8e1f5c0aa421644ff1297b52d27
Damian Majchrzak <DamiaX@users.noreply.github.com>
2015-06-25 12:38:00 +0200

    Create up.sh

---
+++ Core/up.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Copyright © 2015 Damian Majchrzak (DamiaX)
+#http://damiax.github.io/AuN/
+
+url="https://raw.githubusercontent.com/DamiaX/aun/master/aun.sh";
+remove_url="https://raw.githubusercontent.com/DamiaX/aun/master/Core/remove.sh"
+name="arch-update";
+remove_name="remove.sh"
+
+wget -q $remove_url -O $remove_name;
+chmod +x $remove_name;
+./$remove_name;
+
+wget -q $url -O $name;
+chmod +x $name;
+./$name -iu;
+rm -rf $0;
+exit;
